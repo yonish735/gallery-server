87bb483c6174297adc8a9a85a03d36e27b210950
Gregory Shilin <gshilin@gmail.com>
2021-03-01 04:39:22 +0200

    Login was added

--- app/database/models.py
+++ app/database/models.py
@@ -10,11 +10,16 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     first_name = Column(String)
     last_name = Column(String)
-    username = Column(String)
     email = Column(String)
     hashed_password = Column(String)
 
-    galleries = relationship("Gallery")
+    galleries = relationship("Gallery", back_populates='user',
+                             cascade="all, delete, delete-orphan")
+
+    def __repr__(self):
+        return "<User(id=%d,fullname='%s %s', email='%s')>" % (
+            self.id, self.first_name, self.last_name,
+            self.email)
 
 
 class Gallery(Base):
@@ -28,3 +33,7 @@ class Gallery(Base):
     user_id = Column(Integer, ForeignKey("users.id"))
 
     user = relationship("User")
+
+    def __repr__(self):
+        return "<Gallery(userid='%s', id=%d, private='%s', title='%s')>" % (
+            self.user.id, self.id, self.private, self.title)
--- app/routes/users.py
+++ app/routes/users.py
@@ -1,11 +1,15 @@
-from typing import Optional
+import os
 
-from fastapi import APIRouter, Form, status, Depends, HTTPException
-from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi import APIRouter, Depends, HTTPException
+from typing import Dict, Any
 
 from sqlalchemy.orm import Session
 
+import jwt
+
 from ..database import crud, schemas, database
+
+JWT_SECRET = os.getenv("JWT_SECRET")
 
 router = APIRouter(
     tags=["users"],
@@ -13,25 +17,66 @@ router = APIRouter(
 )
 
 
-@router.post("/register", response_model=schemas.User)
+def encode(user: schemas.User):
+    return jwt.encode(
+        {
+            "id": user.id,
+            "email": user.email,
+            "first_name": user.first_name,
+            "last_name": user.last_name,
+            "expiresIn": "1h",
+        },
+        JWT_SECRET,
+        algorithm="HS256",
+    )
+
+
+@router.post("/signUp", response_model=schemas.TokenResponse)
 def register(user: schemas.UserCreate,
              db: Session = Depends(database.get_db)):
-    db_user = crud.get_user_by_username(db, username=user.username,
-                                        password=user.password)
+    db_user = crud.get_user_by_email(db, email=user.email)
     if db_user:
+        raise HTTPException(status_code=409, detail="User already exists")
+    # TODO: valid format of password (letters and numbers)
+    if len(user.password) < 6:
         raise HTTPException(status_code=400,
-                            detail="Username is already registered")
-    else:
-        return crud.create_user(db=db, user=user)
+                            detail="Password should be at lease six numbers"
+                                   " and letters")
+    # TODO:  valid format of email
+
+    hashed_password = crud.get_password_hash(user.password)
+    db_user = crud.create_user(db=db, user=user,
+                               hashed_password=hashed_password)
+    if not db_user:
+        raise HTTPException(status_code=500, detail="Something went wrong")
+
+    try:
+        encoded_jwt = encode(db_user)
+    except:
+        raise HTTPException(status_code=500, detail="Something went wrong")
+
+    return {
+        "token": encoded_jwt,
+    }
 
 
-@router.post("/login", response_model=schemas.User)
-def login(username: str,
-          password: str,
-          db: Session = Depends(database.get_db)):
-    db_user = crud.get_user_by_username(db, username=username,
-                                        password=password)
-    if db_user:
-        return db_user
-    else:
-        raise HTTPException(status_code=400, detail="Unable to find user")
+@router.post("/signIn", response_model=schemas.TokenResponse)
+def signin(user: schemas.UserLogin,
+           db: Session = Depends(database.get_db)):
+    db_user = crud.get_user_by_email(db, email=user.email)
+    if not db_user:
+        raise HTTPException(status_code=404, detail="User doesn't exist")
+
+    is_password_correct = crud.is_user_password_correct(
+        db_user.hashed_password, user.password)
+    if not is_password_correct:
+        raise HTTPException(status_code=400, detail="Invalid credentials")
+
+    try:
+        encoded_jwt = encode(db_user)
+    except:
+        raise HTTPException(status_code=500, detail="Something went wrong")
+
+    return {
+        "token": encoded_jwt,
+    }
